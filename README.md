a1c7a55b079ed085908f84c2641097166078dd7f
Belike81 <belike81@gmail.com>
2011-11-05 14:47:41 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Simple Chat App
+
+This is a simple chat application that I designed to checkout some push features and to further develop my experience
+with ruby. Feel free to fork it and modify it as you like.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleChat::Application.config.secret_token = '3501f306646293bf18c058483871a6d5f44769dc7b30f084c5c32debac9f920976c4c47bf2eeb5c64aad41532e8a4b9eca2889648326d839d753d2e1b029757b'
